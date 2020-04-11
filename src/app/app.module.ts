9279367aeec310b0ca5a1ffbc93f495619d2b5bd
MonkeyScript <monkeyscript.dev@gmail.com>
2020-04-11 22:33:48 +0530

    Added workspace layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { MonkeyChartsModule } from '../../projects/monkey-charts/src/lib/monkey-charts.module'
@@ -13,7 +13,7 @@ import { MonkeyChartsModule } from '../../projects/monkey-charts/src/lib/monkey-
   imports: [
     BrowserModule,
     MonkeyChartsModule,
-    AppRoutingModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
